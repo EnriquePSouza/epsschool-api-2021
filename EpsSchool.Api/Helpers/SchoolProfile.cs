8ae07cdbb753699417ffba67d50f91bf8b7d177f
EnriquePSouza <enriquepdsz@gmail.com>
2021-03-14 12:15:28 -0300

    Added DateTime Extension

--- EpsSchool.Api/Helpers/SchoolProfile.cs
+++ EpsSchool.Api/Helpers/SchoolProfile.cs
@@ -13,6 +13,10 @@ namespace EpsSchool.Api.Helpers
                     dest => dest.Nome,
                     opt => opt.MapFrom(src => $"{src.Nome} {src.Sobrenome}")
                 )
+                .ForMember(
+                    dest => dest.Idade,
+                    opt => opt.MapFrom(src => src.DataNascimento.GetCurrentAge())
+                );
             ;
         }
     }
